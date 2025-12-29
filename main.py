5794018a47c07b035c2b72f3bf5057ce8831fb7c
JamesPiaggio <james.piaggio13@gmail.com>
2025-12-28 22:15:09 -0500

    Added comments

--- main.py
+++ main.py
@@ -1,28 +1,41 @@
 from stats import word_count, character_count, sort_characters
 
+# A function that takes a text file and makes the content readable
 def get_book_text(f):
     with open(f) as f:
         file_contents = f.read()
     return file_contents
 
+# Variable for the content of Frankenstein
 book_text = get_book_text("books/frankenstein.txt")
 
+# Variable that holds the value of the word_count function imported from stats.py
 words = word_count(book_text)
+
+# Variable that holds the value of the character_count function imported from stats.py
 characters = character_count(book_text)
 
+# A helper function to assist with sorting
 def sort_on(items):
     return items["num"]
 
+# Variable holding a sorted list of dictionaries containing the number of each character
 sorted_char = sort_characters(characters)
+
+# Method to sort characters from largest to smallest
 sorted_char.sort(reverse=True, key=sort_on)
 
+# Variable to hold the length of sorted_char list
 length_sorted_char = len(sorted_char)
 
+# Prints the results of bookbot in an easy to read format
 print("============ BOOKBOT ============\n", 
 "Analyzing book found at books/frankenstein.txt...\n", 
-"----------- Word Count ----------\n", 
+"----------- Word Count ----------\n",
+# Lists total number of words in book
 f"Found {words} total words\n", 
 "--------- Character Count -------\n")
-for i in range(length_sorted_char):
+# For loop to iterate over list of dictionaries to print each in clean format
+for i in range(length_sorted_char): 
     print(f"{sorted_char[i]["char"]}: {sorted_char[i]["num"]}")
 print("============= END ==============")
--- stats.py
+++ stats.py
@@ -1,32 +1,47 @@
+# Function that gives a total word count from a book
 def word_count(book_text):
+    # Method splits each word into a separate string then gives length
     count = len(book_text.split())
     return count
 
+# Function that gives a total character count of a book and records each time character appears
 def character_count(book_text):
+    # Puts all text in lowercase
     lowered = book_text.lower()
+    # Declared empty dictionary
     letter_counts = {}
+    # For loop to go through each letter in text
     for letter in lowered:
+        # If letter is already in dictionary, add 1
         if letter in letter_counts:
             letter_counts[letter] += 1
+        # Else create entry and set to 1
         else:
             letter_counts[letter] = 1
     return letter_counts
 
+# Function that sorts characters in a list of dictionaries with keys and removes "banned" characters
 def sort_characters(characters):
+    # Declared empty list of dictionaries for characters
     dictionary_list = [
         {
             "char": "", "num": 0
             }
         ]
+    # Declared empty list of dictionaries for "banned" characters
     banned_list = [
         {
             "char": "", "num": 0
             }
         ]
+    # For loop to go through each dictionary entry in character list
     for character in characters:
+        # If character is part of the alphabet, add to dictionary_list
         if character.isalpha() == True:
             dictionary_list.append({"char": character, "num": characters[character]})
+        # Else add to banned_list
         else:
             banned_list.append({"char": character, "num": characters[character]})
+    # Removes empty entries in dictionary_list
     dictionary_list.remove({"char": "", "num": 0})
     return dictionary_list
