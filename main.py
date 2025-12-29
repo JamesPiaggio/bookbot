8b709ddc57b2c4c377532ac54b532ecbedbe6370
JamesPiaggio <james.piaggio13@gmail.com>
2025-12-28 22:42:57 -0500

    Application now accepts CLI arguments

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 from stats import word_count, character_count, sort_characters
+import sys
+
+# Checks if the correct amount of arguments were passed
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 
 # A function that takes a text file and makes the content readable
 def get_book_text(f):
@@ -6,8 +12,8 @@ def get_book_text(f):
         file_contents = f.read()
     return file_contents
 
-# Variable for the content of Frankenstein
-book_text = get_book_text("books/frankenstein.txt")
+# Variable for the content of book passed via CLI argument
+book_text = get_book_text(sys.argv[1])
 
 # Variable that holds the value of the word_count function imported from stats.py
 words = word_count(book_text)
@@ -30,7 +36,7 @@ length_sorted_char = len(sorted_char)
 
 # Prints the results of bookbot in an easy to read format
 print("============ BOOKBOT ============\n", 
-"Analyzing book found at books/frankenstein.txt...\n", 
+f"Analyzing book found at {sys.argv[1]}...\n", 
 "----------- Word Count ----------\n",
 # Lists total number of words in book
 f"Found {words} total words\n", 
